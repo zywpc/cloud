08f4eb0c96e70e57e3e0948d55b3f72916a33a09
zywpc <114007406+zywpc@users.noreply.github.com>
2022-09-21 17:47:15 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,9 @@
+var user = document.getElementById("user")
+var pass = document.getAnimations("pass")
+function login(){
+    if (pass == 2013){
+        if (user = "admin"){
+            location.reload("")
+        }
+    }
+}
